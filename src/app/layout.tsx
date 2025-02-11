e6ced89c123f88be12fced5212fcb5d5b0f31d03
Pradeep yadav <pradeep20020102@gmail.com>
2025-02-12 00:00:22 +0545

    layout done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="relative w-full flex justify-center items-center">
         <Navbar/>
+        </div>
         {children}
       </body>
     </html>
